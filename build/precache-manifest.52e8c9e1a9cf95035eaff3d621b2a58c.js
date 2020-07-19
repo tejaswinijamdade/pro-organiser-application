5438819a880389859b63aee770b9398466ebf175
Tejaswini Jamdade <59727422+tejaswinijamdade@users.noreply.github.com>
2020-07-19 22:59:16 +0530

    Add files via upload

---
+++ build/precache-manifest.52e8c9e1a9cf95035eaff3d621b2a58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bc69dc7f8697082f9fd350af231b01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edc5f16d8394f6367e5",
+    "url": "/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "fe9b886a1a0e1e5ddda0",
+    "url": "/static/css/main.4c6aa902.chunk.css"
+  },
+  {
+    "revision": "6edc5f16d8394f6367e5",
+    "url": "/static/js/2.03204b21.chunk.js"
+  },
+  {
+    "revision": "5a679c6850c87830997be51c063e1968",
+    "url": "/static/js/2.03204b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9b886a1a0e1e5ddda0",
+    "url": "/static/js/main.39d4fb32.chunk.js"
+  },
+  {
+    "revision": "e9faf038bb1020780332",
+    "url": "/static/js/runtime-main.2069ec65.js"
+  }
+]);
